6cba3c1ca7d2544dec108d88656059698ade63a7
khalid ait bellahs <kaitbellahs@gmail.com>
2022-09-25 18:40:43 -0300

    Update environment

--- gcloud-local-deploy.ps1
+++ gcloud-local-deploy.ps1
@@ -8,26 +8,30 @@ if($args.count -gt 0){
     }
 }
 
+echo "apply environment: $environment"
+
 kubectl -n ikomida delete secret environment
 kubectl -n ikomida-worker delete secret environment
 kubectl -n ikomida-job delete secret environment
-echo "apply environment: $environment"
 if($environment -eq 1){
+    echo "apply homologation settings"
     kubectl apply -f k8s-hmlg/namespace.yaml
-    kubectl apply -f k8s-hmlg
     kubectl -n ikomida create secret generic environment --from-env-file ./k8s-hmlg/environment-secret.env
     kubectl -n ikomida-worker create secret generic environment --from-env-file ./k8s-hmlg/environment-secret.env
     kubectl -n ikomida-job create secret generic environment --from-env-file ./k8s-hmlg/environment-secret.env
+    kubectl apply -f k8s-hmlg
 }elseif($environment -eq 2){
+    echo "apply production settings"
     kubectl apply -f k8s/namespace.yaml
-    kubectl apply -f k8s
     kubectl -n ikomida create secret generic environment --from-env-file ./k8s/environment-secret.env
     kubectl -n ikomida-worker create secret generic environment --from-env-file ./k8s/environment-secret.env
     kubectl -n ikomida-job create secret generic environment --from-env-file ./k8s/environment-secret.env
+    kubectl apply -f k8s-hmlg
 }else{
+    echo "apply development settings"
     kubectl apply -f k8s-dev/namespace.yaml
-    kubectl apply -f k8s-dev
     kubectl -n ikomida create secret generic environment --from-env-file ./k8s-dev/environment-secret.env
     kubectl -n ikomida-worker create secret generic environment --from-env-file ./k8s-dev/environment-secret.env
     kubectl -n ikomida-job create secret generic environment --from-env-file ./k8s-dev/environment-secret.env
+    kubectl apply -f k8s-hmlg
 }
