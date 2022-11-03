52df0312b939ff7ecc89d37dd5f8d57b14958fb3
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-11-03 02:19:52 -0300

    Update

--- db/migration/v1.1.3/up.sql
+++ db/migration/v1.1.3/up.sql
@@ -13,3 +13,20 @@ ALTER TABLE products ADD orderTypes JSON NULL;
 ALTER TABLE vendorSettings ADD orderTypes JSON NULL;
 
 ALTER TABLE vendorSettings ADD tip INTEGER NULL;
+
+ALTER TABLE
+    `orders` MODIFY COLUMN `status` enum(
+        'WAITING_PAYMENT',
+        'OPEN',
+        'ACCEPTED',
+        'WAITING_DELIVERY',
+        'WAITING_PICKUP',
+        'WAITING_LOCAL',
+        'IN_DELIVERY',
+        'IN_TABLE_DELIVERY',
+        'DELIVERED',
+        'IN_DISPUTE',
+        'CANCELED'
+    ) NULL;
+
+ALTER TABLE coupons ADD orderTypes JSON NULL;
