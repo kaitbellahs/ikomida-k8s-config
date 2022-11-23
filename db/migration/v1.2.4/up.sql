9f3c9be084b1b55031f347f32f1b86f930a8e3c3
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-11-23 13:17:11 -0300

    Add app icon, description and links

---
+++ db/migration/v1.2.4/up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `apps` ADD `icon` VARCHAR(255);
+
+ALTER TABLE `apps` ADD `description` VARCHAR(3500);
+
+ALTER TABLE `apps` ADD `androidLink` VARCHAR(255);
+
+ALTER TABLE `apps` ADD `iosLink` VARCHAR(255);
